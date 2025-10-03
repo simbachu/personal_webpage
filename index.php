eb9f8bfc3264b2d18fb1885450517a5e6c2cf162
JG <simbachu@gmail.com>
2025-10-03 15:51:07 +0200

    Add cache-busting to CSS, convert tags to semantic lists with hover effects

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jennifer Gott</title>
     <link rel="stylesheet" href="fonts/inter.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <header>
